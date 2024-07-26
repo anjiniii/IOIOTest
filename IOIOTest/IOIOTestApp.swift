a001abce92d2bee0b60d06203f5a06a56f3bb1e5
Anjin Lee <anjin720jin@naver.com>
2024-07-26 11:03:04 +0900

    first commit

---
+++ IOIOTest/IOIOTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOIOTestApp.swift
+//  IOIOTest
+//
+//  Created by Anjin on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct IOIOTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
